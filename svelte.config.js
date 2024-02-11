ba99347f79c28abed921de76e674b1720ca83a1d
javaj0hn <johnlongbeachny@gmail.com>
2024-02-11 18:25:25 -0500

    Use dynamic build

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,9 @@
-//import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
-    paths: {
-      base: process.env.NODE_ENV === 'production' ? '/osrs-music-guess' : '',
-  }
+    adapter: adapter()
   },
   preprocess: vitePreprocess()
 };
