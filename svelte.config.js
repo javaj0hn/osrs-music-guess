f65c2d5ea1fec8fd18b9c05c7f8976da3e8deca9
javaj0hn <johnlongbeachny@gmail.com>
2024-02-11 19:33:17 -0500

    Static Adapter config options

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,13 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: undefined,
+      precompress: false,
+      strict: true
+    }),
     paths: {
       base: process.env.NODE_ENV === 'production' ? '/osrs-music-guess' : '',
   }
