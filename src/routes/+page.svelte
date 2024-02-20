cf6101a8365374468e17fea79bafe1ae9da8f6bf
javaj0hn <johnlongbeachny@gmail.com>
2024-02-20 00:22:53 -0500

    Radio buttons instead

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,7 @@
     let currentSong = null;
     let userGuess = null;
     let userStatus = null;
+    let randomSongs = []
 
     // function that returns a random song
     async function getRandomSong() {
@@ -16,8 +17,26 @@
         userStatus = null;
         // reset user guess
         userGuess = null;
-        // set random song
-        currentSong = songs.songs[Math.floor(Math.random() * songs.songs.length)];
+        // reset random songs
+        randomSongs = []
+        // reset current song
+        currentSong = null;
+        // clear radio buttons
+        let radioButtons = document.querySelectorAll('input[name="song"]');
+        radioButtons.forEach(radio => {
+            radio.checked = false;
+        });
+
+        // get 4 random songs & all should be unique
+        while (randomSongs.length < 4) {
+            let randomSong = songs.songs[Math.floor(Math.random() * songs.songs.length)];
+            if (!randomSongs.includes(randomSong)) {
+                randomSongs.push(randomSong)
+            }
+        }
+
+        // set random song answer
+        currentSong = randomSongs[Math.floor(Math.random() * randomSongs.length)];
 
         // reset audio player
         let audio = document.getElementById("song");
@@ -28,10 +47,13 @@
     async function guessSong() {
         console.log(currentSong.title);
 
+        // get user guess from radio button
+        userGuess = document.querySelector('input[name="song"]:checked').value;
+        console.log(userGuess);
+
         if (userGuess.toLowerCase() === currentSong.title.toLowerCase()) {
             userStatus = true;
-        } else {            
-            document.getElementById('songInput').value = '';
+        } else {
             userStatus = false;
         }
     }
@@ -53,7 +75,7 @@
     </button>
     {/if}
     {#if currentSong !== null}
-    {#if userStatus}    
+    {#if userStatus}
     <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
         <strong class="font-bold block">Well done! You guessed the song correctly!</strong>
         <div class="block">Unlock Details: {currentSong.UnlockDetails}</div>
@@ -73,12 +95,17 @@
     </div>
     {/if}
     <div class="flex items-center">
-        <input id="songInput" class="border border-gray-700 rounded-lg p-2 m-2 text-white bg-gray-800" type="text" list="songs" placeholder="Song Title..." bind:value={userGuess} on:keydown={handleEnter} />
+        {#each randomSongs as song}
+        <input type="radio" id={song.title} name="song" value={song.title} />
+        <label for={song.title} class="ml-1 mr-2 text-white">{song.title}</label>
+        {/each}
         <datalist id="songs">
             {#each songs.songs as song}
             <option value={song.title} />
             {/each}
         </datalist>
+    </div>
+    <div class="mt-4 flex items-center">
         <button class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex items-center ml-2" on:click={guessSong}>
             <Fa class="mr-2" icon={faLightbulb} />
             <span>Guess</span>
